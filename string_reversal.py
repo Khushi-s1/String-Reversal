577b019d52f14760a512e20536147d0149f33fc3
Khushi-s1 <137272351+Khushi-s1@users.noreply.github.com>
2024-06-10 04:00:31 +0530

    string_reversal.py

--- string_reversal.py
+++ string_reversal.py
@@ -1,6 +1,6 @@
-Level:1 Task-1 String Reversal
+*Level:1 Task-1 String Reversal
 
-Create a Python function that takes a string as input and returns the reverse of that string. For example, if the input is "hello, " the function should return "olleh."
+Create a Python function that takes a string as input and returns the reverse of that string. For example, if the input is "hello, " the function should return "olleh."*
 
 def reverse_string(input_string):
     return input_string[::-1]
